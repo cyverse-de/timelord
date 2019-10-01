814ac0bbb96d127e30fb01e9e2a1f84da7f22b3a
John Wregglesworth <wregglej@iplantcollaborative.org>
2019-10-01 14:32:05 -0700

    Remove K8s API-related code

--- go.mod
+++ go.mod
@@ -3,35 +3,29 @@ module github.com/cyverse-de/timelord
 go 1.12
 
 require (
-	github.com/cloudflare/cfssl v0.0.0-20180323000720-5d63dbd981b5
 	github.com/cyverse-de/configurate v0.0.0-20170404195203-7cb335597f9b
-	github.com/cyverse-de/model v0.0.0-20180604190733-9435ba8c8862
-	github.com/fsnotify/fsnotify v1.4.7
-	github.com/go-redis/redis v6.12.0+incompatible
-	github.com/hashicorp/hcl v0.0.0-20170509225359-392dba7d905e
-	github.com/imdario/mergo v0.3.7 // indirect
+	github.com/cyverse-de/model v0.0.0-20180604190733-9435ba8c8862 // indirect
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/hashicorp/hcl v0.0.0-20170509225359-392dba7d905e // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/lib/pq v0.0.0-20170324204654-2704adc878c2
-	github.com/machinebox/graphql v0.2.2
-	github.com/magiconair/properties v0.0.0-20170321093039-51463bfca257
-	github.com/mitchellh/mapstructure v0.0.0-20170422000251-cc8532a8e9a5
-	github.com/pelletier/go-buffruneio v0.2.0
-	github.com/pelletier/go-toml v0.0.0-20170516171430-5c26a6ff6fd1
+	github.com/magiconair/properties v0.0.0-20170321093039-51463bfca257 // indirect
+	github.com/mitchellh/mapstructure v0.0.0-20170422000251-cc8532a8e9a5 // indirect
+	github.com/pelletier/go-buffruneio v0.2.0 // indirect
+	github.com/pelletier/go-toml v0.0.0-20170516171430-5c26a6ff6fd1 // indirect
 	github.com/pkg/errors v0.0.0-20170505043639-c605e284fe17
 	github.com/sirupsen/logrus v1.4.2
-	github.com/spf13/afero v1.2.2
-	github.com/spf13/cast v1.1.0
-	github.com/spf13/jwalterweatherman v0.0.0-20170510083831-8f07c835e5cc
-	github.com/spf13/pflag v1.0.3
+	github.com/spf13/afero v1.2.2 // indirect
+	github.com/spf13/cast v1.1.0 // indirect
+	github.com/spf13/jwalterweatherman v0.0.0-20170510083831-8f07c835e5cc // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
 	github.com/spf13/viper v0.0.0-20170417080815-0967fc9aceab
 	github.com/streadway/amqp v0.0.0-20180528204448-e5adc2ada8b8
-	golang.org/x/crypto v0.0.0-20190926114937-fa1a29108794 // indirect
-	golang.org/x/sys v0.0.0-20190922100055-0a153f010e69
-	golang.org/x/text v0.3.2
-	golang.org/x/time v0.0.0-20190921001708-c4c64cad1fd0 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/sys v0.0.0-20190922100055-0a153f010e69 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/cyverse-de/messaging.v4 v4.0.0-20180605152013-44770d49ac4d
-	gopkg.in/cyverse-de/model.v3 v3.0.0-20180604190733-9435ba8c8862
-	gopkg.in/yaml.v2 v2.2.2
-	k8s.io/api v0.0.0-20190918155943-95b840bb6a1f
-	k8s.io/apimachinery v0.0.0-20190913080033-27d36303b655
-	k8s.io/client-go v0.0.0-20190918160344-1fbdaa4c8d90
+	gopkg.in/cyverse-de/model.v3 v3.0.0-20180604190733-9435ba8c8862 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 	"net"
 	"net/http"
 	"net/url"
-	"os"
-	"path/filepath"
 	"strconv"
 	"time"
 
@@ -23,9 +21,6 @@ import (
 	_ "github.com/lib/pq"
 
 	"gopkg.in/cyverse-de/messaging.v4"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 const defaultConfig = `db:
@@ -115,7 +110,7 @@ func ConfigureUserLookups(cfg *viper.Viper) error {
 
 // SendKillNotification sends a notification to the user telling them that
 // their job has been killed.
-func SendKillNotification(j *Job, k8s *K8sClient, killNotifKey string) error {
+func SendKillNotification(j *Job, killNotifKey string) error {
 	subject := fmt.Sprintf(KillSubjectFormat, j.Name)
 	endtime, err := time.Parse(TimestampFromDBFormat, j.PlannedEndDate)
 	if err != nil {
@@ -156,7 +151,7 @@ func SendWarningNotification(j *Job) error {
 	return sendNotif(j, j.Status, subject, msg)
 }
 
-func sendWarning(db *sql.DB, vicedb *VICEDatabaser, k8s *K8sClient, warningInterval int64, warningKey string) {
+func sendWarning(db *sql.DB, vicedb *VICEDatabaser, warningInterval int64, warningKey string) {
 	jobs, err := JobKillWarnings(db, warningInterval)
 	if err != nil {
 		log.Error(err)
@@ -217,72 +212,17 @@ func main() {
 	log.SetReportCaller(true)
 
 	var (
-		err        error
-		kubeconfig *string
-		cfg        *viper.Viper
+		err error
+		cfg *viper.Viper
 
 		notifPath       = "/notification"
 		configPath      = flag.String("config", "/etc/iplant/de/jobservices.yml", "The path to the YAML config file.")
 		expvarPort      = flag.String("port", "60000", "The path to listen for expvar requests on.")
-		namespace       = flag.String("namespace", "vice-apps", "The namespace that VICE analyses run in.")
 		appExposerBase  = flag.String("app-exposer", "http://app-exposer", "The base URL for the app-exposer service.")
 		killNotifKey    = flag.String("kill-notif-key", "killnotifsent", "The key for the annotation detailing whether the notification about job termination was sent.")
 		warningInterval = flag.Int64("warning-interval", 60, "The number of minutes in advance to warn users about job kills.")
 		warningSentKey  = flag.String("warning-sent-key", "warningsent", "The key for the annotation detailing whether the job termination warning was sent.")
 	)
-
-	// if cluster is set, then
-	if cluster := os.Getenv("CLUSTER"); cluster != "" {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	} else {
-		// If the home directory exists, then assume that the kube config will be read
-		// from ~/.kube/config.
-		if home := os.Getenv("HOME"); home != "" {
-			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-		} else {
-			// If the home directory doesn't exist, then allow the user to specify a path.
-			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-		}
-	}
-
-	// Print error and exit if *kubeconfig is not empty and doesn't actually
-	// exist. If *kubeconfig is blank, then the app may be running inside the
-	// cluster, so let things proceed.
-	if *kubeconfig != "" {
-		_, err = os.Stat(*kubeconfig)
-		if err != nil {
-			if os.IsNotExist(err) {
-				log.Fatalf("config %s does not exist", *kubeconfig)
-			}
-			log.Fatal(errors.Wrapf(err, "error stat'ing the kubeconfig %s", *kubeconfig))
-		}
-	}
-
-	var config *rest.Config
-	if *kubeconfig != "" {
-		config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
-		if err != nil {
-			log.Fatal(errors.Wrapf(err, "error building config from flags using kubeconfig %s", *kubeconfig))
-		}
-	} else {
-		// If the home directory doesn't exist and the user doesn't specify a path,
-		// then assume that we're running inside a cluster.
-		config, err = rest.InClusterConfig()
-		if err != nil {
-			log.Fatal(errors.Wrapf(err, "error loading the config inside the cluster"))
-		}
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		log.Fatal(errors.Wrap(err, "error creating clientset from config"))
-	}
-
-	k8s := &K8sClient{
-		clientset: clientset,
-		namespace: *namespace,
-	}
-
 	flag.Parse()
 
 	// make sure the configuration object has sane defaults.
@@ -388,10 +328,10 @@ func main() {
 
 		for {
 			// 1 hour warning
-			sendWarning(db, vicedb, k8s, *warningInterval, *warningSentKey)
+			sendWarning(db, vicedb, *warningInterval, *warningSentKey)
 
 			// 1 day warning
-			sendWarning(db, vicedb, k8s, 1440, "onedaywarning")
+			sendWarning(db, vicedb, 1440, "onedaywarning")
 
 			jl, err = JobsToKill(db)
 			if err != nil {
@@ -421,7 +361,7 @@ func main() {
 						log.Error(errors.Wrapf(err, "error terminating analysis '%s'", j.ID))
 					}
 
-					if err = SendKillNotification(&j, k8s, *killNotifKey); err != nil {
+					if err = SendKillNotification(&j, *killNotifKey); err != nil {
 						log.Error(errors.Wrapf(err, "error sending notification that %s has been terminated", j.ID))
 					}
 
