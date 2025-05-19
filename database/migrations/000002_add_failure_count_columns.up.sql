3a3af15627d1be3af395101dfdda2bc9a0c72ce1
John Wregglesworth <wregglej@cyverse.org>
2025-05-19 10:42:47 -0700

    Remove database directory because it's redundant and therefore confusing

--- database/migrations/000002_add_failure_count_columns.up.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE IF EXISTS notif_statuses
-    ADD COLUMN IF NOT EXISTS hour_warning_failure_count INT NOT NULL DEFAULT 0,
-    ADD COLUMN IF NOT EXISTS day_warning_failure_count INT NOT NULL DEFAULT 0,
-    ADD COLUMN IF NOT EXISTS kill_warning_failure_count INT NOT NULL DEFAULT 0;
